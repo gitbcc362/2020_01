fa05f783d34ba6816e79e9fc16c901a38cf9a1b5
jonatasouzac <jonata@lonewolfcorp.com>
2021-04-17 12:25:20 -0300

    removendo arquivos desnecessarios

--- groups/group3/Tp1 - Kafka/backend/dist/data.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.jobs = void 0;
-exports.jobs = [
-    { title: "Desenvolvedor full stack", url: "ibm.com", country: "IN" },
-    { title: "Desenvolvedor Back-End", url: "ibm.com", country: "IN" },
-];
-//# sourceMappingURL=data.js.map
